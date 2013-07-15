407f1e8a1bfa3ab4f07fe37056cb0a50c44ef6af
Sean Grove <s@bushi.do>
2013-07-14 20:55:09 -0700

    Update the compiler to provide source map location for defn, let, and param instances

--- src/clj/cljs/compiler.clj
+++ src/clj/cljs/compiler.clj
@@ -45,6 +45,10 @@
 
 (defonce ns-first-segments (atom '#{"cljs" "clojure"}))
 
+(defn keep-track [info]
+  ;(spit "tmp/emit-history.txt" (str info "\n") :append true)
+  nil)
+
 (defn munge
   ([s] (munge s js-reserved))
   ([s reserved]
@@ -208,13 +212,20 @@
               (let [minfo {:gcol  @*cljs-gen-col*
                            :gline @*cljs-gen-line*
                            :name  var-name}]
-                (update-in m [line]
+                ; Dec the line number for 0-indexed line numbers
+                ; expected in chrome source maps. Seems to be right.
+                (update-in m [(dec line)]
                   (fnil (fn [m]
                           (update-in m [(or column 0)]
                             (fnil (fn [v] (conj v minfo)) [])))
                     (sorted-map)))))))))
-    (when-not (= :statement (:context env))
-      (emit-wrap env (emits (munge info))))))
+    ; We need a way to write vars out to source maps and javascript
+    ; without getting wrapped in an emit-wrap call, otherwise we get
+    ; e.g. (function greet(return x, return y) {})
+    (if (:just-munge? info)
+      (emits (munge info))
+      (when-not (= :statement (:context env))
+        (emit-wrap env (emits (munge info)))))))
 
 (defmethod emit :meta
   [{:keys [expr meta env]}]
@@ -372,7 +383,30 @@
 (defn emit-fn-method
   [{:keys [type name variadic params expr env recurs max-fixed-arity]}]
   (emit-wrap env
-             (emitln "(function " (munge name) "(" (comma-sep (map munge params)) "){")
+             (emits "(function ")
+             ; TODO: Emit source-map for this inner declaration? Maybe
+             ; unecessary.
+             ;                 hello.core.greet = (function greet(){})
+             ; e.g. Do we need source-map for this ---------^?
+             ; If so, we can't just munge the name and spit out a
+             ; string.
+             (emits (munge name))
+             (emits "(")
+             (doseq [param params]
+               ; associate enough with the param to get emit to treat
+               ; it as a :var
+               (emit (assoc param
+                       :op :var
+                       :env (merge env
+                                   {:line (:line param)
+                                    :column (:column param)})
+                       :info {:name (:name param)
+                              :just-munge? true}))
+               ; Avoid extraneous comma (function greet(x, y, z,)
+               (when-not (= param (last params))
+                 (emits ",")))
+             (emits "){")
+             (emitln)
              (when type
                (emitln "var self__ = this;"))
              (when recurs (emitln "while(true){"))
@@ -528,7 +562,13 @@
                                                       (gensym (str (:name %) "-")))
                                              bindings)))]
       (doseq [{:keys [init] :as binding} bindings]
-        (emitln "var " (munge binding) " = " init ";"))
+        (emitln "var " (merge  {:env (merge env {:line (:line binding)
+                                                 :column (:column binding)})
+                                :op :var
+                                :info {:name (:name binding)
+                                       :just-munge? true
+                                       :line (:line binding)
+                                       :column (:column binding)}}) " = " init ";"))
       (when is-loop (emitln "while(true){"))
       (emits expr)
       (when is-loop
